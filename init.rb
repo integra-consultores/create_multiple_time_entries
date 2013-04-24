37f37db2fcfd2c35b65794a445d564f973efe1c7
bishma <bishma@79a4dcc5-733e-4cb7-aff4-656d42035e48>
2013-04-24 21:18:13 +0000

    Agregada estructura para desarrollo de plugin create_multiples_time_entries
    git-svn-id: svn://svn/integra/redmine-plugins/create_multiples_time_entries@1 79a4dcc5-733e-4cb7-aff4-656d42035e48

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :create_multiples_time_entries do
+  name 'Create Multiples Time Entries plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
