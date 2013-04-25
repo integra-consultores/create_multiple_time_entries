93c7775f20d5c3c50bb0476204e17af77866871b
bishma <bishma@79a4dcc5-733e-4cb7-aff4-656d42035e48>
2013-04-25 16:58:25 +0000

    Routes updated to work with Bitnami Redmine
    git-svn-id: svn://svn/integra/redmine-plugins/create_multiples_time_entries@6 79a4dcc5-733e-4cb7-aff4-656d42035e48

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
-RedmineApp::Application.routes.draw do
-  resources :projects, :only => [] do
-    get 'bulk_time_entries/new', :to => "bulk_time_entries#new", :as => "new_bulk_time_entries"
-    post 'bulk_time_entries', :to => "bulk_time_entries#create", :as => "bulk_time_entries"  
-  end  
+for prefix in [ActionController::Base.config.relative_url_root, "/"] do
+  scope prefix do
+    resources :projects, :only => [] do
+      get 'bulk_time_entries/new', :to => "bulk_time_entries#new", :as => "new_bulk_time_entries"
+      post 'bulk_time_entries', :to => "bulk_time_entries#create", :as => "bulk_time_entries"  
+    end  
+  end
 end
